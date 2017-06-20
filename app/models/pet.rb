26688cee20a8f8501ca0d0ad23e7666615f7c1d1
irvin373 <irvin373@gmail.com>
2017-06-20 13:06:25 -0400

    alerts internship

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,4 +2,12 @@ class Pet < ApplicationRecord
   belongs_to :client
   has_many :internships
   has_many :registers
+
+  def dischargeDate
+  	date = Time.now.to_date
+  	if self.internships.size > 0
+  		date = self.internships.last.dischargeDate
+  	end
+  	date
+  end
 end
